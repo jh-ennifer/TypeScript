dc3a24e32b3578dc1a862dd3d0d232a6729231c1
Jhennifer <jhenniferrodrigues200119@gmail.com>
2025-08-24 19:13:30 -0300

    Projeto "Conversão de temperatura"

--- src/conversorGrau/index.ts
+++ src/conversorGrau/index.ts
@@ -1,5 +1,22 @@
-const hello = (name: string): string => {
-  return `Olá, ${name}! 🚀`;
-};
+function converter(valor :number, tipo :String) {
+  if (tipo === "CtoF") {
+    return (valor * 9) / 5 + 32;
+  } else if (tipo === "FtoC") {
+    return ((valor - 32) * 5) / 9;
+  } else {
+    throw new Error("Tipo de conversão inválido!");
+  }
+}
 
-console.log(hello("TypeScript"));
+type Conversao = {valor: number, tipo: "CtoF" | "FtoC"}
+
+const resultados :Conversao[] = [
+  { valor: 0, tipo: "CtoF" },
+  { valor: 100, tipo: "CtoF" },
+  { valor: 32, tipo: "FtoC" },
+  { valor: 212, tipo: "FtoC" }
+];
+
+resultados.forEach(({ valor, tipo }) => {
+  console.log(`${valor} ${tipo} → ${converter(valor, tipo)}`);
+});
